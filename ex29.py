2e2363d14df37bccc5aba7907d6af9a8a6e7edce
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-03-19 11:02:08 -0300

    Update ex29.py

--- ex29.py
+++ ex29.py
@@ -12,3 +12,32 @@ def cadastrar_pessoa(cadastros):
     curso = input("Digite seu curso:")
     cadastros.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso})
     print("Cadastro realizado com sucesso!")
+
+def ver_cadastro(cadastros):
+    if not cadastros:
+        print("\n Não há cadastros no sistema")
+    else:
+        print("\n ------LISTA DE CADASTROS------")
+        
+        for i, pessoa in enumerate (cadastros, 1):
+            print(f"{i} . Nome: {pessoa['Nome']}, Idade: {pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
+
+def main():
+    cadastros = []
+    while True:
+        exibir_menu()
+        opção = input("Escolha uma opção: ")
+        if opção == "1":
+            cadastrar_pessoa (cadastros)
+        elif opção == "2":
+            ver_cadastro (cadastros)
+        
+        elif opção == "3":
+            print("Obrigado por utilizar o sistema!")
+            break
+
+        else:
+            print("Opção incorreta! Tente novamente.")
+
+if __name__== "_main_":
+    main()
